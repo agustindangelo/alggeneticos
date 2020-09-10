088cd0b395990393855e884e05b7f4ffc75a0c15
Agustin DAngelo <agustindangelo2113@gmail.com>
2020-09-10 14:51:09 -0300

    cambiso estéticos, titulo al gráfico

--- web-app-fractales/apps/glaciares.py
+++ web-app-fractales/apps/glaciares.py
@@ -72,85 +72,37 @@ layout = html.Div([
 )
 
 def update_figure(anio, semilla, longitud):
-    if (anio==1950):
-        altura_min=9
-        altura_max=10
-        variabilidad=0.7
-        temp="4.9°C"
-    elif (anio==1960):
-        altura_min=8.8696
-        altura_max=9.8696
-        variabilidad=0.6913
-        temp="5°C"
-    elif (anio==1970):
-        altura_min=8.8044
-        altura_max=9.8044
-        variabilidad=0.6869
-        temp="6.05°C"
-    elif (anio==1980):
-        altura_min=8.6088
-        altura_max=9.6088
-        variabilidad=0.6739
-        temp="6.2°C"
-    elif (anio==1990):
-        altura_min=8.45232
-        altura_max=9.45232
-        variabilidad=0.6635
-        temp="6.32°C"
-    elif (anio==2000):
-        altura_min=8.0872
-        altura_max=9.0872
-        variabilidad=0.6391
-        temp="6.6°C"
-    elif (anio==2010):
-        altura_min=7.8916
-        altura_max=8.8916
-        variabilidad=0.6261
-        temp="6.75°C"
-    elif (anio==2020):
-        altura_min=7.5656
-        altura_max=8.5656
-        variabilidad=0.6
-        temp="7°C"
-    elif (anio==2030):
-        altura_min=6.9136
-        altura_max=7.9136
-        variabilidad=0.5609
-        temp="7.5°C"
-    elif (anio==2040):
-        altura_min=6.2616
-        altura_max=7.2616
-        variabilidad=0.5175
-        temp="8°C"
-    elif (anio==2050):
-        altura_min=4.9576
-        altura_max=5.9576
-        variabilidad=0.4306
-        temp="9°C"
-    elif (anio==2060):
-        altura_min=3.6536
-        altura_max=4.6536
-        variabilidad=0.3437
-        temp="10°C"
-    elif (anio==2070):
-        altura_min=3.0016
-        altura_max=4.0016
-        variabilidad=0.3
-        temp="10.5°C"
-    elif (anio==2080):
-        altura_min=1.5672
-        altura_max=2.5672
-        variabilidad=0.2047
-        temp="11.6°C"
-    else:
-        altura_min=0
-        altura_max=1
-        variabilidad=0.1
-        temp="12.8°C"
+    factor_var=0.0869 #cambio de variabilidad x cambio de °C
+    factor_altura=9.13 #cambio de altura x cambio de °C
+    temp_default=7 #en °C para el año 2020
+    altura_min_defaut=53 #en metros para el año 2020
+    altura_max_default=60 #en metros para el año 2020
+    variabilidad_default=0.6 #para el año 2020
 
-    #para mostrar medidas de altura mas realistas (en metros)
-    altura_min=altura_min*7 
-    altura_max=altura_max*7
+    temps = {
+        1950: 4.9,
+        1960: 5,
+        1970: 6.05,
+        1980: 6.2,
+        1990: 6.32,
+        2000: 6.6,
+        2010: 6.75,
+        2020: 7,
+        2030: 7.5,
+        2040: 8,
+        2050: 9,
+        2060: 10,
+        2070: 10.5,
+        2080: 11.6,
+        2090: 12.8
+    }
+
+    temp = temps[anio]
+    #asignacion de parametros para configurar la figura    
+    altura_min=altura_min_defaut + factor_altura*(temp_default-temp)
+    altura_max=altura_max_default + factor_altura*(temp_default-temp)
+    variabilidad=variabilidad_default + factor_var*(temp_default-temp)
+    temp=f'{temp}°C'
 
     Z = generar_terreno(
             tamaño=(longitud, longitud),
@@ -202,14 +154,17 @@ def update_figure(anio, semilla, longitud):
 
     fig = go.Figure(data = superficies)
 
-    fig.update_layout(scene = dict(
+    fig.update_layout(
+                    title=f'Año {anio}, temperatura promedio: {temp}',
+                    titlefont={'size':30},
+                    scene = dict(
                         xaxis = dict(ticks='',visible=False),
                         yaxis = dict(ticks='',visible=False),
                         zaxis = dict(ticks='',visible=False),
                     ),
                     width=1000,
                     height=700,
-                    margin=dict(t=0, r=100, l=100, b=0)
+                    margin=dict(t=50, r=100, l=100, b=0)
                 )
 
     return fig
